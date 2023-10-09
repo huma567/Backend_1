20de4ff9b44a719a88c0732e417f266ba9ed3a4b
huma567 <khadimhuma4@gmail.com>
2023-10-09 10:11:14 +0700

    Q6

---
+++ Question6.py
@@ -0,0 +1,10 @@
+num_list = [70, 60, 50, 40, 36, 30, 20, 10]
+
+count = 0
+
+for index, num in enumerate(num_list):
+    if num == 36:
+        print("Number found at position:", index)
+        count += 1 
+
+print("Total count of number 36:", count)
